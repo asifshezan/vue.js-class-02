9a28da56a3e4578944bd30ea665c4e9257d4b6f1
unknown <asifshezan7@gmail.com>
2022-04-26 09:56:46 +0600

    components & Make child components under home component

---
+++ src/components/Home.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>Home Component</h1>
+        <Users />
+        <products />
+    </div>
+</template>
+<script>
+import Products from './Products.vue';
+import Users from './Users.vue'
+
+
+export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name:'Home',
+    components:{
+    Users,
+        Products,
+    }
+};
+</script>
+
+<style scoped>
+    div{
+        background-color:rgb(218, 137, 232);
+        height: 500px;
+    }
+</style>
